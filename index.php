b393df43f863ab1e8120e59c0a50ab459caab90a
Albertasion <93448198+Albertasion@users.noreply.github.com>
2024-04-14 23:04:28 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -28,12 +28,15 @@ $key = 0;
 $dir_files_pages = 'data\all_products_garden';
 $files_in_directory = scandir($dir_files_pages);
 
-// foreach ($files_in_directory as $key=>$files) {
-//   if ($files[0]!=='.' && $files[1]!=='.') {
-  $doc = file_get_contents($dir_files_pages.'/'.'page_10179.html');
+$flag = 0;
+foreach ($files_in_directory as $key=>$files) {
+  if ($flag>1000) break;
+ 
+  if ($files[0]!=='.' && $files[1]!=='.') {
+  // $doc = file_get_contents($dir_files_pages.'/'.'page_10179.html');
 
 
-// $doc = file_get_contents($dir_files_pages.'/'.$files);
+$doc = file_get_contents($dir_files_pages.'/'.$files);
 $document = phpQuery::newDocument($doc);
 
 
@@ -56,6 +59,7 @@ $url = $document->find('link[rel="canonical"]');
 $url = pq($url);
 $url = $url->attr('href');
 $product[$key]['url'] = $url;
+echo $url.'<br>';
 ///////////////////////////////////////////////посилання на сторінку
 
 
@@ -90,9 +94,7 @@ foreach($images_block as $key=>$value){
 }
 
 
-//пустышки картинки logo-  
-
-
+//пустышки картинки logo-,  assproductimage-
 
 
 
@@ -124,70 +126,36 @@ $description_row = [];
 $description_row_value = [];
 //опис 
 $descrition= $document->find('.kh-1xzm1su');
-$row_name = $descrition->find('.kh-10oizfy');
-foreach ($row_name as $key=>$value) {
-  $tr = pq($value)->text();
-  if ($tr =='EAN') continue;
-      echo $tr.'<br>';
-      $description_row[$key] = $tr;
+
+
+$row_name = $descrition->find('tr');
+//название значения
+foreach ($row_name as $key1=>$value) {
+  $th = pq($value)->find('th')->text();
+  $tr = pq($value)->find('td')->text();
+
+  $tr = str_replace('mm', 'мм', $tr);
+
+
+
+  $li = pq($value)->find('.kh-16zd49f')->html();
+if($li) {
+  $li = str_replace('</span><span>', ' | ', $li);
+  $li = str_replace('mm', 'мм', $li);
+  $description_row_value[$key1] = $li;
 }
-format($description_row);
-// echo $descrition;
-$row_value = $descrition->find('.kh-pkx4zo');
-foreach ($row_value as $key=>$value) {
-  $tr_value = pq($value)->text();
-      echo $tr_value.'<br>';
-      $description_row_value[$key] = $tr_value;
+else {
+  $description_row_value[$key1] = $tr;
 }
-format($description_row_value);
-
-
-// $combine_description = array_combine($description_row, $description_row_value);
-// format($combine_description);
-
-// foreach ($trs as $tr) {
-//     // Делайте что-то с каждым найденным <tr>
-//     $tr = pq($tr);
-//     echo $tr.'<br>';
-//     $description_row[] = $tr;
-// }
-$conn = connect_db();
-$sql_show_columns = "SHOW COLUMNS FROM products";
-$result = $conn->query($sql_show_columns);
-while($row = $result->fetch_assoc()) {
-echo $row['Field'].'<br>';
-      // $existing_columns[] = $row['Field'];
+ 
+      // echo $th.'----------' .$tr.'<br>';
+      $description_row[$key1] = $th;
+      
     }
 
-// if ($result->num_rows > 0) {
-//   // Преобразование результатов в массив
-//   $existing_columns = array();
-//   while($row = $result->fetch_assoc()) {
-//     $existing_columns[] = $row['Field'];
-//   }
 
-//   // Проверка и добавление новых значений
-//   foreach ($combine_description as $column => $value) {
-//     if (!in_array($column, $existing_columns)) {
-//       // Колонка существует, добавляем значение под ней
-//       $sql = "UPDATE products SET $column = '$value'";
-//       if ($conn->query($sql) === TRUE) {
-//         echo "Value '$value' added under column $column successfully<br>";
-//       } else {
-//         echo "Error adding value under column $column: " . $conn->error . "<br>";
-//       }
-//     } else {
-//       echo "Column $column does not exist<br>";
-//     }
-//   }
-// } else {
-//   echo "No columns found in the table";
-// }
-
-$conn->close();
-
-
-
+$combine_description = array_combine($description_row, $description_row_value);
+format($combine_description);
 
 
 
@@ -197,10 +165,12 @@ $conn->close();
 // $sql = "INSERT INTO products (name, sku, brand, image, origsku, url)
 // VALUES ('$product_name', '$sku', '$brand', '$images_str', '$originals_sku_arr_str', '$url')";
 // $con->query($sql);  
+$flag++;
 $document->unloadDocument();
-//   } 
-// }
-format($product);
+echo "<hr>";
+  } 
+}
+// format($product);
 
 
 
